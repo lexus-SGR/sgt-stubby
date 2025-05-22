94fcdf80f02b1dab0a1af58a564f0dc725f7163b
lexus-SGR <whittakertech0@gmail.com>
2025-05-22 23:27:19 +0300

    Update truth.js

--- commands/truth.js
+++ commands/truth.js
@@ -1,13 +1,28 @@
 module.exports = {
   name: 'truth',
   description: 'Play Truth or Dare (truth question)',
-  async execute(msg) {
+
+  async execute(sock, msg, args, from) {
     const truths = [
       'What is your biggest fear?',
       'Have you ever lied to your best friend?',
-      'What’s your biggest secret?'
+      'What’s your biggest secret?',
+      'Have you ever had a crush on someone in this group?',
+      'Have you ever stolen something?',
+      'What’s the most embarrassing thing you’ve done?',
+      'Who was your first kiss?',
+      'Have you ever faked being sick to skip school?',
+      'What’s the last lie you told?',
+      'Who do you secretly admire here?',
+      'What’s your guilty pleasure?',
+      'If you had to date someone here, who would it be?',
     ];
+
     const question = truths[Math.floor(Math.random() * truths.length)];
-    msg.reply(`TRUTH: ${question}`);
+
+    await sock.sendMessage(from, {
+      text: `🗣️ *TRUTH*: ${question}`,
+      quoted: msg,
+    });
   }
 };
