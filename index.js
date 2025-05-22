2cc257beb160eb95165085741bf4b1e2e71ade2b
lexus-SGR <whittakertech0@gmail.com>
2025-05-22 16:33:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Fatuma WhatsApp Bot is running!"));
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running on port " + (process.env.PORT || 3000));
+});
+
 require('events').EventEmitter.defaultMaxListeners = 50;
 
 const OWNER_JID = "255760317060@s.whatsapp.net"; // Owner number
