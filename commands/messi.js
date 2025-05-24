0ca21b210293e07b8124f8555f1d92c910024762
lexus-SGR <whittakertech0@gmail.com>
2025-05-24 11:12:40 +0300

    Update messi.js

--- commands/messi.js
+++ commands/messi.js
@@ -1,24 +1,44 @@
 const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
   name: "messi",
-  description: "Send a random image of Lionel Messi",
+  description: "Download and send a random image of Lionel Messi",
   async execute(sock, msg, args, from) {
+    const accessKey = "3_ZLDJCDvk4xsHStdG5K_E4az24833uZPMtcY34BBUc"; // Unsplash Access Key
+
     await sock.sendMessage(from, {
-      react: {
-        text: "🐐",
-        key: msg.key
-      }
+      react: { text: "🐐", key: msg.key }
     });
 
     try {
-      const res = await axios.get(`https://api.unsplash.com/photos/random?query=lionel messi&client_id=${process.env.UNSPLASH_KEY}`);
-      await sock.sendMessage(from, {
-        image: { url: res.data.urls.small },
-        caption: "Lionel Messi - GOAT"
-      }, { quoted: msg });
-    } catch {
-      await sock.sendMessage(from, { text: "Could not find Messi image." }, { quoted: msg });
+      const res = await axios.get(`https://api.unsplash.com/photos/random?query=lionel messi&client_id=${accessKey}`);
+      const imageUrl = res.data.urls.full;
+
+      const tempFilePath = path.resolve(__dirname, "../media/messi.jpg");
+      const writer = fs.createWriteStream(tempFilePath);
+      const imageResponse = await axios.get(imageUrl, { responseType: "stream" });
+      imageResponse.data.pipe(writer);
+
+      writer.on("finish", async () => {
+        await sock.sendMessage(from, {
+          image: fs.readFileSync(tempFilePath),
+          mimetype: "image/jpeg",
+          fileName: "lionel_messi.jpg",
+          caption: "Lionel Messi - GOAT"
+        }, { quoted: msg });
+
+        fs.unlinkSync(tempFilePath);
+      });
+
+      writer.on("error", async () => {
+        await sock.sendMessage(from, { text: "❌ Failed to download the image." }, { quoted: msg });
+      });
+
+    } catch (err) {
+      console.error(err);
+      await sock.sendMessage(from, { text: "❌ Couldn't fetch Messi image." }, { quoted: msg });
     }
   }
 };
