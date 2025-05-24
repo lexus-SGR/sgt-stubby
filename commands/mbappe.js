0a0610b6a6b2f3ae0700b7618f1f101df341fde7
lexus-SGR <whittakertech0@gmail.com>
2025-05-24 11:11:39 +0300

    Update mbappe.js

--- commands/mbappe.js
+++ commands/mbappe.js
@@ -1,24 +1,44 @@
 const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
   name: "mbappe",
-  description: "Send a random image of Kylian Mbappé",
+  description: "Download and send a random image of Kylian Mbappé",
   async execute(sock, msg, args, from) {
+    const accessKey = "3_ZLDJCDvk4xsHStdG5K_E4az24833uZPMtcY34BBUc"; // Unsplash Access Key
+
     await sock.sendMessage(from, {
-      react: {
-        text: "🚀",
-        key: msg.key
-      }
+      react: { text: "🚀", key: msg.key }
     });
 
     try {
-      const res = await axios.get(`https://api.unsplash.com/photos/random?query=kylian mbappe&client_id=${process.env.UNSPLASH_KEY}`);
-      await sock.sendMessage(from, {
-        image: { url: res.data.urls.small },
-        caption: "Kylian Mbappé - Speed Demon"
-      }, { quoted: msg });
-    } catch {
-      await sock.sendMessage(from, { text: "Couldn't find Mbappé image." }, { quoted: msg });
+      const res = await axios.get(`https://api.unsplash.com/photos/random?query=kylian mbappe&client_id=${accessKey}`);
+      const imageUrl = res.data.urls.full;
+
+      const tempFilePath = path.resolve(__dirname, "../media/mbappe.jpg");
+      const writer = fs.createWriteStream(tempFilePath);
+      const imageResponse = await axios.get(imageUrl, { responseType: "stream" });
+      imageResponse.data.pipe(writer);
+
+      writer.on("finish", async () => {
+        await sock.sendMessage(from, {
+          image: fs.readFileSync(tempFilePath),
+          mimetype: "image/jpeg",
+          fileName: "kylian_mbappe.jpg",
+          caption: "Kylian Mbappé - Speed Demon"
+        }, { quoted: msg });
+
+        fs.unlinkSync(tempFilePath);
+      });
+
+      writer.on("error", async () => {
+        await sock.sendMessage(from, { text: "❌ Failed to download the image." }, { quoted: msg });
+      });
+
+    } catch (err) {
+      console.error(err);
+      await sock.sendMessage(from, { text: "❌ Couldn't fetch Mbappé image." }, { quoted: msg });
     }
   }
 };
