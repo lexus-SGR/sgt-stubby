8d390e4ca859429172b8b39e99c368e67b1f1916
lexus-SGR <whittakertech0@gmail.com>
2025-05-25 12:03:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -32,8 +32,7 @@ const AUTO_VIEW_ONCE = process.env.AUTO_VIEW_ONCE === "on" ? true : false;
 const AUTO_VIEW_STATUS = process.env.AUTO_VIEW_STATUS === "on" ? true : false;
 const ANTILINK_ENABLED = process.env.ANTILINK === "on";
 const ANTILINK_ACTION = process.env.ANTILINK_ACTION || "remove";
-
-const { antilinkCommands, antiLinkCheck } = require("./antilink"); // Ensure path is correct
+const handleAntiLink = require('./antilink');
 
 async function startBot() {
   const { state, saveCreds } = await useMultiFileAuthState("./auth");
